782819f764a7c7778ba69c52cfdae87072ee4e99
ellaisa <ellaisa@bk.ru>
2020-11-05 14:42:46 +0300

    my second project

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Эллаиса Даценко on 15.08.2020.
+//  Copyright © 2020 Эллаиса Даценко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
